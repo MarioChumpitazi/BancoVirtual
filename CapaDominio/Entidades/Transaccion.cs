d0723634a665ef52f9a0463faadc1c816ac09f29
Mario Chumpitazi <chumpitazi.98.mario@gmail.com>
2020-11-03 17:52:50 -0500

    completo capa dominio

--- CapaDominio/Entidades/Transaccion.cs
+++ CapaDominio/Entidades/Transaccion.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,23 +15,29 @@ namespace CapaDominio.Entidades
         private DateTime fecha;
         private float monto;
         private bool tipo;  //0 entre cuentas y 1 a terceros
+        private bool moneda;
         private int valoracion;
 
-        private List<Usuario> listaUsuarios;
+
+        public bool Moneda { get => moneda; set => moneda = value; }
 
         public string TransaccionID { get => transaccionID; set => transaccionID = value; }
         public DateTime Fecha { get => fecha; set => fecha = value; }
         public float Monto { get => monto; set => monto = value; }
         public bool Tipo { get => tipo; set => tipo = value; }
         public int Valoracion { get => valoracion; set => valoracion = value; }
-        public int CodigoDeMovimiento { get => codigoDeMovimiento; set => codigoDeMovimiento = value; }
-        public List<Usuario> ListaUsuario { get { return listaUsuarios; } set { listaUsuarios = value; } }
 
-        
-        public void realizarTransaccion()
+        private Usuario usuario;
+
+        public Usuario Usuario
         {
-
+            get { return usuario; }
+            set { usuario = value; }
         }
+
+        private int clave;
+
+        //regla 4
         public bool verificarMonto(float monto, Cuenta cuenta)
         {
             if (monto <= cuenta.Saldo)
@@ -39,7 +46,7 @@ namespace CapaDominio.Entidades
                 return false;
         }
 
-
+        //regla 2
         public float verificarComision(bool tipo)
         {
             float comision = 0f;
@@ -49,8 +56,10 @@ namespace CapaDominio.Entidades
                 comision = 0.15f;
             return comision;
         }
-        public float calcularMontoTotal(bool tipo, float montoAux)
+        //regla 7
+        public float calcularMontoTotal()
         {
+            float montoAux=0f;
             float montoTotal= 0f;
             if (tipo== false)
             {
@@ -64,6 +73,7 @@ namespace CapaDominio.Entidades
             return montoTotal;
         }
 
+        //regla 14
         public bool validarValoracion(int valorizacion)
         {
             if (valoracion >= 1 && valoracion <= 5)
@@ -72,25 +82,27 @@ namespace CapaDominio.Entidades
                 return false;
         }
 
-        /*
-        public float calcularTransferencia(float monto, string tipomoneda1)
-        {
-            if (tipomoneda1.Equals("DOLAR"))
-            {
-                monto *= 3.45f;
-            }
-            return monto;
-        }
+       
 
-        
+        //regla 5
+        public float verificarMoneda(Transaccion transaccion, Cuenta cuentaDestino)
+        {
+            if(transaccion.moneda = false  && cuentaDestino.Moneda==false)
+                return monto;
+            else
+            {
+                return monto *= 3.45f;
+            }
+        }
+        /*
 
         public bool verificarCodigo(string codigoaux, string transaccionID2)
         {
             transaccionID = transaccionID2;
             return (transaccionID == transaccionID2);
         }
-        */
-        public bool existeUsuario(Usuario usuarioAux)
+        
+        public bool existeUsuario(Usuario usuario)
         {
             string existenteID;
             foreach (Usuario usuario in listaUsuarios)
@@ -103,29 +115,69 @@ namespace CapaDominio.Entidades
             }
             return false;
         }
-
-        public Usuario buscarUsuarioID(Usuario usuario)
+        public Usuario buscarUsuario(Usuario usuario)
         {
-            if (existeUsuario(usuario))
+            return banco.buscarUsuarioID(usuario);
+
+        }
+        */
+        public int generarClaveTransaccion()
+        {
+            var rand = new Random();
+            return clave=rand.Next(1000, 9999); //genera clave entre 1000 y 9999
+        }
+
+        //regla 6
+        public void verificarIntentos(Cuenta cuenta, int intentos)
+        {
+            if (intentos >= 3)
+                cuenta.Estado = false;
+        }
+        
+        public void verificarClaveTransaccion(Banco banco, Cuenta cuenta, Cuenta cuentaDestino, int clave, int intentos)
+        {
+            if (cuenta.Estado == true)
             {
-                return usuario;
+                if (clave == generarClaveTransaccion())
+                {
+                    realizarTransaccion(banco,cuenta,cuentaDestino,monto);
+                }
+                else
+                {
+                    intentos++;
+                    verificarIntentos(cuenta, intentos);
+                }
             }
-
-            return null;
+            
         }
 
-        public bool verificarIntentos()
+        //regla 1
+        public Cuenta buscarCuenta(Banco banco, Cuenta cuenta)
         {
-            return false;
+            if (tipo == true)
+            {
+                usuario = banco.buscarUsuario(usuario);
+                cuenta = usuario.buscarCuentaID(cuenta);
+            }
+            else
+            {
+                cuenta = usuario.buscarCuentaID(cuenta);
+            }
+            
+            return cuenta;
         }
-
-        public void generarClaveTransaccion()
+        public void realizarTransaccion(Banco banco, Cuenta cuenta, Cuenta cuentaDestino, float monto)
         {
+            buscarCuenta(banco, cuenta);
+            //verificarClaveTransaccion(banco,cuenta,cuentaDestino,clave,intentos)
+            actualizarSaldos(cuenta, cuentaDestino,monto);
 
         }
-
-        public void verificarClaveTransaccion()
-        { 
+        public void actualizarSaldos(Cuenta cuenta, Cuenta cuentaDestino, float montoTransferir)
+        {
+            float monto=calcularMontoTotal();
+            float saldoCuenta = cuenta.Saldo - monto;
+            float saldoCuentaDestino = cuentaDestino.Saldo + monto;
         }
     }
 }
